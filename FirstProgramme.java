e70b2d8a88cf95d248bf702fefee6d7830b1679a
naimesh9280 <naimesh9280@yahoo.co.in>
2022-01-09 16:47:49 +0000

    this is my first commit

---
+++ FirstProgramme.java
@@ -0,0 +1,10 @@
+package com.firstprogramme;
+
+public class FirstProgramme {
+    public static void main(String[] args) {
+        System.out.print("Naimesh");
+        System.out.print("Patel");
+        System.out.println("hello");
+
+    }
+}
---
+++ ManualTesting.java
@@ -0,0 +1,24 @@
+package com.firstprogramme;
+
+public class ManualTesting {
+    public static void main(String[] args) {
+        System.out.println("Manual Testing Topics");
+        System.out.println("What is Software");
+        System.out.println("Software Risk");
+        System.out.println("Software Testing");
+        System.out.println("Benefits of Software Testing");
+        System.out.println("Qualification of tester");
+        System.out.println("What is Manual Testing");
+        System.out.println("Software Developement Life Cycle");
+        System.out.println("Software Testing Life Cycle");
+        System.out.println("Principle of the software testing");
+        System.out.println("Test plan");
+        System.out.println("Test Scenario");
+        System.out.println("Test Case");
+        System.out.println("Eqvivalence Class Partitioning");
+        System.out.println("Boundrry value analysis");
+        System.out.println("What is a Bug");
+        System.out.println("Testing Levels");
+    }
+
+}
